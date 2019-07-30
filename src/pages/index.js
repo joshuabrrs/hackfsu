1f9702a9b48a16272cbd49fa78990c514a91c706
Andrew Sosa <asosa04@harris.com>
2019-07-30 14:54:40 -0400

    Remove outdated components, add tachyons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="center flex flex-column justify-center items-center mw6 vh-75">
+      <h1 className="lh-copy f3 f2-m f1-l">FEATHERHACKS</h1>
+      <p>FEATHER STATE | TALLAHACKEE, FL</p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
