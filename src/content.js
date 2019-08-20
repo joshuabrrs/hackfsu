c556aaa1fb39fe64cca7fafb9ffe1dfd3041ef44
joshuabrrs <jgb16@my.fsu.edu>
2019-08-20 12:37:17 -0400

    added faq

--- src/content.js
+++ src/content.js
@@ -1,13 +1,13 @@
 export default {
   main: {
-    eventName: "featherhacks",
-    subheader: "FEATHER STATE | TALLAHACKEE, FL",
-    date: "FALL 20XX",
+    eventName: "HackFSU",
+    subheader: "FLORIDA STATE | TALLAHASSEE, FL",
+    date: "OCTOBER 18 - 20, 2019",
     applyText: "Apply Now",
-    applyLink: "https://hackfsu.com",
-    emailDisplay: "info@featherhacks.com",
-    emailLink: "mailto:info@featherhacks.com",
-    footerText: "Made with ❤️ in Tallahackee, FL",
+    applyLink: "https://airtable.com/shruU4BGbwpaTMYEL",
+    emailDisplay: "hackfsu.team@gmail.com",
+    emailLink: "mailto:hackfsu.team@gmail.com",
+    footerText: "Made with ❤️ in Tallahassee, FL",
   },
   details: {
     header: "What's a hackathon?",
@@ -38,25 +38,57 @@ export default {
     header: "FAQs",
     items: [
       {
-        header: "A place to create.",
-        body: `At hackathons, you have the ability to build on any idea you can come
-            up with. Web design, frontend/backend development, iOS and Android
-            apps, hardware hacks, and server management are just a few of the
-            possibilities.`,
+        header: "When and where.",
+        body: `HackFSU 6 will take place from October 18 - 20, 2019 at Florida State University in Tallahassee, FL.
+         We’ll release more detailed information closer to the event.
+        `,
       },
       {
-        header: "An opportunity for experience.",
-        body: ` Workshops, tech talks, and access to mentors allows both beginning and
-        experienced students to learn new technologies. Head over to our
-        sponsor tables and find out about potential internship and career
-        opportunities.`,
+        header: "How long is it.",
+        body: `You will have 36 hours to build but we will also have opening and closing ceremonies as well as the project expo. 
+        Prepare to spend your weekend with us starting Friday night and we’ll have you out around midday on Sunday. 
+        A more detailed schedule will be released closer to the event.`,
       },
       {
-        header: "A chance to collaborate.",
-        body: `Working alongside equally passionate people allows you to become
-        encouraged to keep building and learning. You’ll be amazed what you
-        can accomplish as a team.`,
+        header: "Am I invited.",
+        body: `Anyone that is currently a college student or that has graduated in the past year is invited. 
+        High schoolers can go ahead and apply and we’ll notify you if any problems come about.
+        Not a student? Become a mentor! Give back to the community by helping hackers learn and build something awesome.`,
       },
+
+      {
+        header: "Food.",
+        body: `Food. Enough free grub to keep you hacking for 36 hours. Dietary restrictions? No problem, we got you.`,
+      },
+      {
+        header: "How do teams work.",
+        body: `Teams have a maximum of 4. If you don’t have a team, no sweat, we will have a mixer for you to discuss ideas and meet other hackers to join forces.
+        `,
+      },   {
+        header: "What's it gonna cost me.",
+        body: `Everything we provide including food, wifi, caffeine, swag, and a place to create is free!`,
+      },   {
+        header: "What can I build.",
+        body: `Whatever you can think up! We’ll have mentors to help you with web, mobile, hardware, design, and any development in between!
+        `,
+      },   {
+        header: "What should I bring.",
+        body: `Bring your student ID, laptop, chargers, a change of clothes, your resume, and toiletries. A pillow and sleeping bag may be useful as well. 
+        You do not have to bring a team, idea, food, or any drinks. Please leave anything illegal at home.
+        `,
+      },   {
+        header: "How does registration work.",
+        body: `After you register, you will receive a confirmation email reserving your spot.`,
+      },   {
+        header: "What if this is my first hackathon.",
+        body: `Awesome! The best way to learn is by building. There will be mentors to help you every step of the way. We all start somewhere, why not here?
+        `,
+      },   {
+        header: "Wait but I have another question.",
+        body: `Shoot us an email at hackfsu.team@gmail.com or find us on Twitter or Facebook. We are happy to help!
+        `,
+      },
+      
     ],
   },
   culture: {
